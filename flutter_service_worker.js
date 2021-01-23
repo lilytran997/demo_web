111629bb5cc9c97fd4554dbfd162ce31e2119403
lilytran997 <lytran997@gmail.com>
2021-01-23 19:08:17 +0700

    push

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -34,9 +34,9 @@ const RESOURCES = {
 "favicon.png": "1038c5e2ef67d1bd710d21ccc5ef4237",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "be8440786de129be6ffb8c566f4d0724",
-"/": "be8440786de129be6ffb8c566f4d0724",
-"main.dart.js": "78dfeca748d10ba7c7167ddd1cffa9c9",
+"index.html": "022bb6aeb6870fe876bcdf98a489aa42",
+"/": "022bb6aeb6870fe876bcdf98a489aa42",
+"main.dart.js": "30c758f798181e722cefd96510335e28",
 "manifest.json": "9a3c437de8d17b66b056cb4f6f565d35",
 "version.json": "8972b18bec7fc8ff91e74c919e6dc4a8"
 };
